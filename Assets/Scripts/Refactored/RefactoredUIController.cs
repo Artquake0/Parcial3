58d6ed5acf52fabe42ee6f7e8000bf723961e6fa
Artquake0 <sofia.gonzalezh@upb.edu.co>
2023-10-28 11:40:53 -0500

    ui controller

--- Assets/Scripts/Refactored/RefactoredUIController.cs
+++ Assets/Scripts/Refactored/RefactoredUIController.cs
@@ -1,8 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RefactoredUIController : UIControllerBase
 {
+    public Text ScoreText;
+    public Text ShotsRemainingText;
+    public GameObject GameOverOverlay;
+
+    void OnEnable()
+    {
+        RefactoredPlayerController.Instance.GameOver += HandleGameOver;
+        RefactoredPlayerController.Instance.OnShot += HandleShot;
+    }
+
+    void OnDisable()
+    {
+        RefactoredPlayerController.Instance.GameOver -= HandleGameOver;
+        RefactoredPlayerController.Instance.OnShot -= HandleShot;
+    }
+
+    void HandleGameOver()
+    {
+        GameOverOverlay.SetActive(true);
+    }
+
+    void HandleShot()
+    {
+        int score = GameController.GetScore();
+        int shotsRemaining = RefactoredPlayerController.Instance.ShotsRemaining;
+
+        ScoreText.text = $"Score: {score}";
+        ShotsRemainingText.text = $"Shots Remaining: {shotsRemaining}";
+    }
     protected override GameControllerBase GameController => throw new System.NotImplementedException();
 }
