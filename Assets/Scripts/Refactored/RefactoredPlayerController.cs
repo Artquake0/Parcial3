d5b11381918da32108bbda703f29577908630344
Artquake0 <sofia.gonzalezh@upb.edu.co>
2023-10-28 09:54:09 -0500

    Avance PlayerController

--- Assets/Scripts/Refactored/RefactoredPlayerController.cs
+++ Assets/Scripts/Refactored/RefactoredPlayerController.cs
@@ -1,9 +1,48 @@
+using System.Windows.Input;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class RefactoredPlayerController : PlayerControllerBase
 {
-    protected override void ProcessShot(Vector3 point)
+    private PlayerController playerController;
+    public GameObject Shot;
+    public float MaxShotDistance;
+    public int MaxShots;
+    public int ShotsRemaining;
+
+    public static RefactoredPlayerController Instance;
+
+    public event GameOverDelegate OnGameOver;
+    public event ProcessShot OnShot;
+
+    void Awake()
     {
-        throw new System.NotImplementedException();
+        Instance = this;
+        playerController = new PlayerController();
     }
+
+    void Update()
+    {
+        if (ShotsRemaining <= 0) return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnShot?.Invoke();
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, MaxShotDistance))
+            {
+                playerController.ProcessShot(hit.point);
+            }
+
+            ShotsRemaining--;
+
+            if (ShotsRemaining <= 0)
+            {
+                OnGameOver?.Invoke();
+            }
+        }
+    }
+
 }
